b0062bc7c22cb5c35e1142b4816932ee22c1158e
Surajit Karuri <surajitkaruri@gmail.com>
2021-08-23 22:10:01 +0530

    Release v2.1 Minor bug fix UI improvement Added feature for choosing the fisrt player

--- react-tic-tac-toe/src/App.js
+++ react-tic-tac-toe/src/App.js
@@ -6,47 +6,78 @@ import resetImg from "./assets/circular-arrow.png";
 import checkWinner from "./winLogic";
 
 function App() {
-  const defaultItemArr = new Array(9).fill(null);
-  const [itemArr, setitemArr] = useState(defaultItemArr);
+  const nullArray = new Array(9).fill(null);
+  const [itemArr, setitemArr] = useState(nullArray);
   const [isCross, setisCross] = useState(true);
   const [winner, setWinner] = useState("");
   const [message, setMessage] = useState("");
+  const [playerSelected, setPlayerSelected] = useState(false);
+
+  const choosePlayer = (e) => {
+    const player = e.target.value;
+    setisCross(player === "cross");
+  };
 
   const clickHandler = (index) => {
-    if(message.includes('won')){
+    if (message.includes("won")) {
       alert("Click on Try Again for a new game");
       return;
     }
-    if(itemArr[index]){
+    if (itemArr[index]) {
       setMessage("Already taken");
       return;
     }
+    setPlayerSelected(true);
     itemArr[index] = isCross ? "cross" : "circle";
-    setitemArr(itemArr);
+    setitemArr([...itemArr]);
     setisCross(!isCross);
-    const winner = checkWinner(itemArr)
-    if(winner){
-      setWinner(winner)
+    const winner = checkWinner(itemArr);
+    if (winner) {
+      setWinner(winner);
       setMessage(`${winner} won`);
-    }else{
-      setMessage("")
+    } else {
+      setMessage("");
     }
   };
 
   const resetHandler = () => {
-    setitemArr(itemArr.fill(null, 0, 9));
+    setitemArr(nullArray);
     setMessage("");
     setWinner("");
     setisCross(true);
+    setPlayerSelected(false);
   };
 
   return (
     <div className="container">
       <h2>a dumb tic-tac-toe game</h2>
+      {!playerSelected && (
+        <div className='radio_palyer'>
+          <input
+            type="radio"
+            name="firstPlayer"
+            id="circle"
+            value="cross"
+            checked={isCross}
+            onChange={choosePlayer}
+          />
+          <label htmlFor="circle">Cross</label>
+          <input
+            type="radio"
+            name="firstPlayer"
+            id="cross"
+            value="circle"
+            checked={!isCross}
+            onChange={choosePlayer}
+          />
+          <label htmlFor="circle">Circle</label>
+        </div>
+      )}
+
       <div className="matrix">
         {itemArr.map((item, index) => (
           <div key={index} className="cell" onClick={() => clickHandler(index)}>
-            {item != null &&
+            {item &&
               (item === "cross" ? (
                 <img src={crossImg} alt="cross" />
               ) : (
@@ -56,28 +87,26 @@ function App() {
         ))}
       </div>
 
-
       <div className="message">
-      {winner? (
-        <div className="success-message">
-          <h3>{message}</h3>
-          <button onClick={resetHandler}>
-          <span>
-            Try again
-          </span>
-            <img src={resetImg} alt="reset-img" className="resetImg"/>
-          </button>
-
-        </div>
-      ):(
-        <>
-        <h5 className="promt-message">{message}</h5>
-        <div className="player">
-          {`${isCross ? "Cross" : "Circle"} is playing`}
-        </div>
-        <button onClick={resetHandler}>Reset</button>
-        </>
-      )}
+        {winner ? (
+          <div className="success-message">
+            <h4>{message}</h4>
+            <button onClick={resetHandler}>
+              <span>Try again</span>
+              <img src={resetImg} alt="reset-img" className="resetImg" />
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="promt-message">
+              <h5>{message}</h5>
+            </div>
+            <div className="player">
+              {`${isCross ? "Cross" : "Circle"} is playing`}
+            </div>
+            <button onClick={resetHandler}>Reset</button>
+          </>
+        )}
       </div>
     </div>
   );
